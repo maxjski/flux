856584f5ac4c31b0cbed0ed4cf19601396af84fa
Maksymilian Jankowski <2619901j@student.gla.ac.uk>
2025-12-24 01:22:42 +0000

    changes for fun

--- src/bin/sender.rs
+++ src/bin/sender.rs
@@ -12,7 +12,7 @@ fn main() -> std::io::Result<()> {
 
     println!("Blasting market data to {}...", multicast_addr);
 
-    let mut sequence_num: u64 = 1;
+    let mut sequence_num = [1u8];
 
     loop {
         // --- MOLDUDP64 PACKET STRUCTURE (20 Bytes) ---
@@ -20,19 +20,19 @@ fn main() -> std::io::Result<()> {
         let mut packet = Vec::with_capacity(20);
 
         // Session ID (just 10 bytes of zeros for now)
-        packet.extend_from_slice(&[0u8; 10]);
+        packet.extend_from_slice(&[0u8; 1]);
 
         // Sequence Number (Critical: Big Endian)
-        packet.extend_from_slice(&sequence_num.to_be_bytes());
+        packet.extend_from_slice(&sequence_num);
 
         // Message Count (1 message)
-        packet.extend_from_slice(&1u16.to_be_bytes());
+        packet.extend_from_slice(&(-1i8).to_be_bytes());
 
         // Send it!
         socket.send_to(&packet, multicast_addr)?;
 
-        println!("Sent Sequence: {}", sequence_num);
-        sequence_num += 1;
+        println!("Sent Sequence: {}", sequence_num[0]);
+        sequence_num[0] += 1;
 
         // Sleep 1 second (slow enough to see with your eyes)
         thread::sleep(Duration::from_secs(1));
